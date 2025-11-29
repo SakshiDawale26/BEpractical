1099d30d40a47977d26e8510879e9ed8004f6e67
Sakshi Dawale <dawalesakshi26@gmail.com>
2025-11-29 23:59:13 +0530

    Add files via upload

---
+++ python/mini project/randomNo.py
@@ -0,0 +1,19 @@
+import random
+target = random.randint(1,100)
+
+while True:
+    userChoice = input("Guess the target or Quite : ")
+    if (userChoice =="Quite"):
+        break
+    
+    userChoice = int(userChoice)
+    if(userChoice == target):
+        print("success : correct Guess! : ")
+        break
+    elif(userChoice < target):
+        print("your number was too small, take a bigger guess..")
+    else:
+        print("your number was too big, take a smaller guess..")
+        
+        
+print("----------GameOver----------")
